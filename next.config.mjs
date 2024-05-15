d6cfc86a503aa9c8ea5e2ff3d9369f1644517365
talaxy <fujianchensiwei@163.com>
2024-05-15 15:19:32 +0800

    feat: 删除 basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/freesia',
     rewrites: () => {
         return [
             {
                 source: '/api/:path*',
                 destination: 'https://blog.talaxy.cn/api/:path*',
-                basePath: false,
             },
         ];
     },
