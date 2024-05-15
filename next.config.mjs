24112e9450d0e828d18cb0aab0880a371840ad7a
talaxy <fujianchensiwei@163.com>
2024-05-15 15:41:26 +0800

    fix: 还原代码

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/freesia',
     rewrites: () => {
         return [
             {
                 source: '/api/:path*',
                 destination: 'https://blog.talaxy.cn/api/:path*',
+                basePath: false,
             },
         ];
     },
