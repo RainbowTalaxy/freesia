981598c8e7e17272acb557d12d3ead0538fab4c8
talaxy <fujianchensiwei@163.com>
2024-05-23 14:21:34 +0800

    chore: 用户界面调整

--- src/app/(site)/user/page.tsx
+++ src/app/(site)/user/page.tsx
@@ -14,16 +14,20 @@ export default async function Page() {
     return (
         <div className="px-[24px] py-[36px] m-auto max-w-[540px]">
             <h1 className="mb-[16px] text-[32px] font-bold">用户登录</h1>
-            <p className="mb-[16px]">
-                当前用户：
-                {userId ?? <span className="text-gray-500">暂未登录</span>}
-            </p>
             {!userId ? (
                 <Suspense>
                     <Login />
                 </Suspense>
             ) : (
-                <Logout />
+                <>
+                    <p className="mb-[16px]">
+                        当前用户：
+                        {userId ?? (
+                            <span className="text-gray-500">暂未登录</span>
+                        )}
+                    </p>
+                    <Logout />
+                </>
             )}
         </div>
     );
