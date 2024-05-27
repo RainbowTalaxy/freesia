0b5bc9a0049af59d6f63adf4850ba39fd8d984f2
talaxy <fujianchensiwei@163.com>
2024-05-27 18:29:47 +0800

    feat: 文章样式

--- src/app/(apps)/luoye/components/Markdown/index.tsx
+++ src/app/(apps)/luoye/components/Markdown/index.tsx
@@ -10,10 +10,16 @@ interface Slug {
 interface Props {
     toc: (slugs: Array<Slug>) => ReactNode;
     children: string;
+    preContent?: ReactNode;
 }
 
-const Markdown = ({ children, toc }: Props) => {
-    return <ReactMarkdown>{children}</ReactMarkdown>;
+const Markdown = ({ children, preContent }: Props) => {
+    return (
+        <article className="article">
+            {preContent}
+            <ReactMarkdown>{children}</ReactMarkdown>
+        </article>
+    );
 };
 
 export default Markdown;
