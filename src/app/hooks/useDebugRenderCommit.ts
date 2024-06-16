a44c138f61514b4161c54c81de7abe342d40721e
talaxy <fujianchensiwei@163.com>
2024-06-16 11:13:26 +0800

    chore: 增加渲染阶段打印

--- src/app/hooks/useDebugRenderCommit.ts
+++ src/app/hooks/useDebugRenderCommit.ts
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { Logger } from '../utils';
 
 const useDebugRenderCommit = (componentName: string) => {
+    Logger.render(componentName);
+
     useEffect(() => {
         Logger.info('🎨', componentName, 'render committed');
     });
